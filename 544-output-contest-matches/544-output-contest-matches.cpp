675d51c9a915416495c9ff92f55e6fff10432015
The-Higgs-Boson <niranjanbodiga3535@gmail.com>
2022-07-01 23:46:00 -0700

    Time: 8 ms (65.08%), Space: 9 MB (96.83%) - LeetHub

--- 544-output-contest-matches/544-output-contest-matches.cpp
+++ 544-output-contest-matches/544-output-contest-matches.cpp
@@ -1,20 +1,26 @@
 class Solution {
 public:
     string arr[5000];
+    
+    void rec(int n) {
+        if(n==1) {
+            return;
+        }
+        int l=0,r=n-1;
+        while(l<r) {
+            arr[l]="("+arr[l]+","+arr[r]+")";
+            l++;
+            r--;
+        }
+        rec(r+1);
+    }
+    
     string findContestMatch(int n) {
         for(int i=0;i<n;i++) {
             arr[i]=to_string(i+1);
             // cout<<arr[i]<<endl;
         }
-        int l=0,r=n-1;
-        while(l<r) {
-            while(l<r) {
-                arr[l]="("+arr[l]+","+arr[r]+")";
-                l++;
-                r--;
-            }
-            l=0;
-        }
+        rec(n);
         return arr[0];
     }
 };
